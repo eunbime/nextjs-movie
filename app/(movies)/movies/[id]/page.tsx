dd1282ecdc7eb8c5ad59b24c51c49b42287754d7
eunbime <ebk1014@gmail.com>
2024-10-30 08:52:30 +0900

    feat: parallel request fetching

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,4 +1,15 @@
-export default async function MovieDetail({ params }) {
-  const { id } = await params;
-  return <h1>Movies {id}</h1>;
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+export default async function MovieDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const movie = await getMovie(id);
+  return <h1>{movie.title}</h1>;
 }
