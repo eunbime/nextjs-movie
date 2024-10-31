c40d8d3bd3729f5db03731199c2a236bf2ca3a5e
eunbime <ebk1014@gmail.com>
2024-11-01 08:17:50 +0900

    feat: error handling

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,8 +1,10 @@
 import { API_URL } from "@/app/(home)/page";
 
 async function getVideos(id: string) {
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  throw new Error("something broke");
+  // const response = await fetch(`${API_URL}/${id}/videos`);
+  // return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
