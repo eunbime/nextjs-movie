7927a717d816da54293da3cfd1eabc04119a7ad0
eunbime <ebk1014@gmail.com>
2024-11-02 18:23:16 +0900

    style: 홈페이지 및 상세페이지 스타일링

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import Navigation from "@/components/navigation";
+import "@/styles/global.css";
 
 export const metadata: Metadata = {
   title: {
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,5 +1,8 @@
 import { API_URL } from "@/app/(home)/page";
 
+import styles from "@/styles/movie-info.module.css";
+import Link from "next/link";
+
 async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
@@ -8,5 +11,21 @@ async function getMovie(id: string) {
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
 
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={styles.container}>
+      <img
+        className={styles.poster}
+        src={movie.poster_path}
+        alt={movie.title}
+      />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>★{movie.vote_average}</h3>
+        <p>{movie.overview}</p>
+        <Link href={movie.homepage} target={"_blank"}>
+          Homepage &rarr;
+        </Link>
+      </div>
+    </div>
+  );
 }
